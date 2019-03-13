2d86bd4678c6979f304636da6a869913e80d3c85
DBogas <dbogas738@gmail.com>
2019-03-13 15:55:16 +0000

    finished giant component calculation

--- homework1/ex3.py
+++ homework1/ex3.py
@@ -1,6 +1,7 @@
 # compute the size of the random networks we generated
 
 import networkx as nx
+import time
 
 def read_graph_from_file(filename):
 
@@ -30,46 +31,43 @@ def read_graph_from_file(filename):
 # sauces:
 #   https://en.wikipedia.org/wiki/Breadth-first_search
 #   https://www.geeksforgeeks.org/connected-components-in-an-undirected-graph/
-def find_connected_components(graph):
-
-    node_queue = list() # node list so BFS progresses
-    visited_nodes = list() # set of visited nodes
-    nodes_to_visit = list(graph.nodes) # list of nodes to visit (guarantees order since we append)
-    components = list() # list of connected components' sizes
-    comp = list()
-    node_queue.append(nodes_to_visit.pop(0))
-
-    while len(nodes_to_visit) > 0:
-        currnode = node_queue.pop(0)
-        comp.append(currnode)
-
-        graph.nodes[currnode]['visited'] = True
-        visited_nodes.append(currnode)
-
-        # add neighbours to queue if not visited or already in queue to get visited
-        neighbours = nx.all_neighbors(graph, currnode)
-        for neighbour in neighbours:
-            if not neighbour in node_queue and graph.nodes[neighbour]['visited'] == False:
-                print currnode," knows ", neighbour, " is not visited or in queue"
-                node_queue.append(neighbour)
-                nodes_to_visit.remove(neighbour)
-
-        # end of component
-        if len(node_queue) == 0 and len(nodes_to_visit) > 0:
-            node_queue.append(nodes_to_visit.pop(0))
-            components.append(comp)
-
-
-    print len(visited_nodes)
-
-    return False
-
-
-
 def calculate_giant_component_size(graph):
 
-    connected_components = find_connected_components(graph)
-    return 1
+    time_start = int(round(time.time() * 1000))
+
+    components = list()
+    node_queue = list()
+    visitedNodes = set()
+    nodes_to_visit = list(graph.nodes)
+    root = nodes_to_visit.pop(0)
+    currNode = root
+    currComp = list()
+    sizes = list()
+    node_queue.append(currNode)
+
+    while len(visitedNodes) < len(list(graph.nodes)):
+
+        currNode = node_queue.pop(0)
+        currComp.append(currNode)
+        visitedNodes.add(currNode)
+        neighbours = nx.all_neighbors(graph,currNode)
+
+        for n in neighbours:
+            if n not in visitedNodes and n not in node_queue and n in nodes_to_visit:
+                node_queue.append(n)
+                nodes_to_visit.remove(n)
+
+        # component end
+        if len(node_queue) == 0:
+            if len(nodes_to_visit) > 0:
+                node_queue.append(nodes_to_visit.pop(0))
+            components.append(currComp)
+            sizes.append(len(currComp))
+            currComp = list()
+
+    time_end = int(round(time.time() * 1000))
+    print "\t%d nodes \n\tin %d ms\n\twith giant component having size %d" % (len(visitedNodes),time_end-time_start,max(sizes))
+    return max(sizes)
 
 if __name__ == '__main__':
     #graph1 = read_graph_from_file("random1.txt")
