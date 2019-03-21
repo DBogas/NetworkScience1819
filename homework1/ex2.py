e5fb2917a69f5761f9e79aa7de6c7414bf2c932d
DBogas <dbogas738@gmail.com>
2019-03-21 17:33:14 +0000

    fixed nodes begginning in 1 instead of 0

--- homework1/ex2.py
+++ homework1/ex2.py
@@ -8,14 +8,13 @@ p2 = 0.005
 
 def generate_erdos_renyi_graph(size, prob):
     graph = nx.Graph()
-    nodes = (x for x in range(0,size))
+    nodes = list(x for x in range(1,size+1))
     graph.add_nodes_from(nodes)
-
     # triangular superior matrix (undirected graph)
-    for i in range(0, len(graph.nodes)):
-        for j in range(i+1,len(graph.nodes)):
+    for i in graph.nodes:
+        for j in graph.nodes:
             # not sure if < or <= here
-            if random.uniform(0, 1) < prob:
+            if i != j and random.uniform(0, 1) < prob:
                 graph.add_edge(i,j)
 
     return graph
@@ -36,10 +35,13 @@ def print_graph_to_file(filename,graph):
 
 
 if __name__ == '__main__':
+
     size = 1000
     erdos_renyi_1 = generate_erdos_renyi_graph(size, p1)
     erdos_renyi_2 = generate_erdos_renyi_graph(size, p2)
 
+    print 0 in erdos_renyi_1.nodes
+    print 0 in erdos_renyi_2.nodes
 
 
     print_graph_to_file("random1.txt", erdos_renyi_1)
